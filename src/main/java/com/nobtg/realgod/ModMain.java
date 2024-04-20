4e0b3fc9c767094878ad7ce0a1332f8856115e92
NOBTG <nobtg114514@gmail.com>
2024-04-20 12:44:27 +0800

    re

--- src/main/java/com/nobtg/realgod/ModMain.java
+++ src/main/java/com/nobtg/realgod/ModMain.java
@@ -1,7 +1,6 @@
 package com.nobtg.realgod;
 
 import com.nobtg.realgod.items.RealGodItem;
-import com.nobtg.realgod.utils.client.render.avaritia.AvaritiaShaders;
 import net.minecraft.world.item.Item;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -16,6 +15,5 @@ public class ModMain {
         DeferredRegister<Item> items = DeferredRegister.create(ForgeRegistries.ITEMS, ModMain.modID);
         items.register("real_god", RealGodItem::new);
         items.register(FMLJavaModLoadingContext.get().getModEventBus());
-        AvaritiaShaders.init();
     }
 }
